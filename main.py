a5d1e6fdf0ddccc70c362865f3c274fecc3ed02b
Arsh Singhal <arsh.singhal@drishinfo.com>
2025-04-29 15:39:12 +0530

    Integrated status codes

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status, HTTPException
 from pydantic import BaseModel
 from typing import Optional
 from random import randrange
@@ -25,15 +25,24 @@ class Post(BaseModel):
     published : bool = False
     rating : Optional[int] = None
 
+
 @app.get("/")
 def root():
     return {"message" : "welcome to my api! "}
 
+@app.get("/posts/{id}")     # id field here is a path parameter.
+def get_post(id : int):
+    for post_data in my_post:
+        if post_data['id'] == id:
+            return post_data
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                        detail=f"data not found with id : {id}")
+
 @app.get("/posts")
-def get_posts():
+def get_all_posts():
     return {"data" : my_post}
 
-@app.post('/posts')
+@app.post('/posts', status_code=status.HTTP_201_CREATED)
 def create_post(post : Post):
     datas = post.model_dump()
     datas['id'] = randrange(0,10000000)
