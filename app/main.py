9c47c17da5912e4c07532a330eb59add0a31bb37
Arsh Singhal <singhalarsh2001@gmail.com>
2025-05-25 15:15:12 +0530

    hero section struture with security

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
-from fastapi import FastAPI, Query, HTTPException
+from fastapi import FastAPI, Query, HTTPException, status
 from sqlmodel import SQLModel, select
-from app.models import Hero, HeroUpdate
+from app.models import HeroPublic, HeroUpdate, Hero, HeroCreate
 from app.database import engine, SessionDep
 from typing import Annotated
 
@@ -11,48 +11,50 @@ app = FastAPI()
 def on_startup():
     SQLModel.metadata.create_all(engine)
 
-# Creating a Hero User
-@app.post("/heroes/")
-def create_hero(hero: Hero, session: SessionDep) -> Hero:
-    session.add(hero)
+# Create a Hero
+@app.post("/heroes/", response_model=HeroPublic)
+def create_hero(hero: HeroCreate, session: SessionDep):
+    db_hero = Hero.model_validate(hero)
+    session.add(db_hero)
     session.commit()
-    session.refresh(hero)
-    return hero
+    session.refresh(db_hero)
+    return db_hero
 
-# Returning all Heros
-@app.get("/heroes/")
+# Read Heroes
+@app.get("/heroes/", response_model=list[HeroPublic])
 def read_heroes(
     session: SessionDep,
     offset: int = 0,
     limit: Annotated[int, Query(le=100)] = 100,
-) -> list[Hero]:
+):
     heroes = session.exec(select(Hero).offset(offset).limit(limit)).all()
+    if heroes is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                            detail=f"data not found")
     return heroes
 
-# Getting a Single Hero
-@app.get("/heroes/{hero_id}")
-def read_hero(hero_id: int, session: SessionDep) -> Hero:
+# Read One Hero
+@app.get("/heroes/{hero_id}", response_model=HeroPublic)
+def read_hero(hero_id: int, session: SessionDep):
     hero = session.get(Hero, hero_id)
     if not hero:
         raise HTTPException(status_code=404, detail="Hero not found")
     return hero
 
-# Updating a Hero
-@app.put("/heroes/{hero_id}")
-def update_hero(hero_id: int, hero_update: HeroUpdate, session: SessionDep):
-    hero = session.get(Hero, hero_id)
-    if not hero:
+# Update a Hero
+@app.patch("/heroes/{hero_id}", response_model=HeroPublic)
+def update_hero(hero_id: int, hero: HeroUpdate, session: SessionDep):
+    hero_db = session.get(Hero, hero_id)
+    if not hero_db:
         raise HTTPException(status_code=404, detail="Hero not found")
-
-    hero_data = hero_update.model_dump(exclude_unset=True)
-    hero.sqlmodel_update(hero_data)
-
-    session.add(hero)
+    hero_data = hero.model_dump(exclude_unset=True)
+    hero_db.sqlmodel_update(hero_data)
+    session.add(hero_db)
     session.commit()
-    session.refresh(hero)
-    return hero
+    session.refresh(hero_db)
+    return hero_db
 
-# Deleting a Hero
+# Delete a Hero
 @app.delete("/heroes/{hero_id}")
 def delete_hero(hero_id: int, session: SessionDep):
     hero = session.get(Hero, hero_id)
--- app/models.py
+++ app/models.py
@@ -1,13 +1,25 @@
 from sqlmodel import Field, SQLModel
 
-class Hero(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
+class HeroBase(SQLModel):
     name: str = Field(index=True)
     age: int | None = Field(default=None, index=True)
+
+
+class Hero(HeroBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)
     secret_name: str
-    
-class HeroUpdate(SQLModel):
-    name: str = None
-    age: int = None
-    secret_name: str = None
-    
+
+# Database will automatically create an Id, i don't need to create one
+# I just need to access data via id
+class HeroPublic(HeroBase):
+    id: int
+
+
+class HeroCreate(HeroBase):
+    secret_name: str
+
+
+class HeroUpdate(HeroBase):
+    name: str | None = None
+    age: int | None = None
+    secret_name: str | None = None
