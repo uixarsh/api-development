34891bb5b3b245b056b6b1ed8cb2e8fb4e992e0c
Arsh Singhal <arsh.singhal@drishinfo.com>
2025-05-26 11:36:19 +0530

    handled date time errors

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Query, HTTPException, status
 from sqlmodel import SQLModel, select
-from app.schemas import Post, PostPublic, PostCreate, PostUpdate
+from app.schemas import Post, PostPublic, CreatePost, UpdatePost, User, CreateUser, UserPublic
 from app.database import engine, SessionDep
 from typing import Annotated
 
@@ -12,8 +12,8 @@ def on_startup():
     SQLModel.metadata.create_all(engine)
 
 # Create Post
-@app.post("/posts/", response_model=PostPublic)
-def create_post(create_post: PostCreate, session: SessionDep):
+@app.post("/posts/", response_model=PostPublic, status_code=status.HTTP_201_CREATED)
+def create_post(create_post: CreatePost, session: SessionDep):
     post = Post.model_validate(create_post)
     session.add(post)
     session.commit()
@@ -43,7 +43,7 @@ def read_post(id: int, session: SessionDep):
 
 # Update Post
 @app.put("/posts/{id}", response_model=PostPublic, status_code= status.HTTP_201_CREATED)
-def update_post(id: int, update_post: PostUpdate, session: SessionDep):
+def update_post(id: int, update_post: UpdatePost, session: SessionDep):
     post = session.get(Post, id)
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Data not found")
@@ -62,3 +62,25 @@ def delete_post(id: int, session: SessionDep, status_code=status.HTTP_204_NO_CON
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"data not found with id : {id}")
     session.delete(post)
     session.commit()
+
+# Create User
+@app.post("/users/", response_model=UserPublic, status_code=status.HTTP_201_CREATED)
+def create_user(create_user: CreateUser, session: SessionDep):
+    user = User.model_validate(create_user)
+    session.add(user)
+    session.commit()
+    session.refresh(user)
+    return user
+
+# Read Users
+@app.get("/users/", response_model=list[UserPublic])
+def read_users(
+    session: SessionDep,
+    offset: int = 0,
+    limit: Annotated[int, Query(le=100)] = 100,
+):
+    users = session.exec(select(User).offset(offset).limit(limit)).all()
+    if not users:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                            detail=f"data not found")
+    return users
--- app/schemas.py
+++ app/schemas.py
@@ -1,24 +1,41 @@
-from sqlmodel import Field, SQLModel
-from datetime import timezone
+from sqlmodel import Field, SQLModel, Column, DateTime, func
+from pydantic import EmailStr
 import datetime
 
 class PostBase(SQLModel):
     title: str = Field(default=None, index=True)
     content: str = Field(default=None)
     published: bool = Field(default=False, nullable=True)
-    created_at: datetime.datetime = Field(default_factory = lambda: datetime.datetime.now(timezone.utc))
+    
     
 class Post(PostBase, table=True):
-    id: int | None = Field(default=None, primary_key=True)
+    id: int = Field(default=None, primary_key=True)
+    created_at: datetime.datetime = Field(default_factory=lambda: datetime.datetime.now(datetime.timezone.utc), sa_column=Column(DateTime(timezone=True), server_default=func.now(), nullable=False))
 
 class PostPublic(PostBase):
     id: int
 
-class PostCreate(PostBase):
+class CreatePost(PostBase):
     title: str 
     content: str
 
-class PostUpdate(PostBase):
+class UpdatePost(PostBase):
     title: str | None = None
     content: str | None = None
     published: bool | None = None
+
+
+class UserBase(SQLModel):
+    email : EmailStr = Field(default=None, unique=True)
+    name : str = Field(default=None)
+    
+class User(UserBase, table=True):
+    id : int = Field(default=None, primary_key=True)
+    password : str = Field(default=None)
+    created_at: datetime.datetime = Field(default_factory=lambda: datetime.datetime.now(datetime.timezone.utc), sa_column=Column(DateTime(timezone=True), server_default=func.now(), nullable=False))
+    
+class CreateUser(UserBase):
+    password : str
+
+class UserPublic(UserBase):
+    id : int 
